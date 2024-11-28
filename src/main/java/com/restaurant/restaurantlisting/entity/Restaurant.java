c2a49a5dc68b10f4ed199f2e5039c2e355903b71
git-kkumar <77931687+git-kkumar@users.noreply.github.com>
2024-11-28 22:51:37 +0530

    Entity updated

--- src/main/java/com/restaurant/restaurantlisting/entity/Restaurant.java
+++ src/main/java/com/restaurant/restaurantlisting/entity/Restaurant.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
 @AllArgsConstructor
+@Table(name = "restaurant")
 public class Restaurant {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
