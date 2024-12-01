0e622bb37d18bc935749cca43e9cc868c3a085a0
git-kkumar <77931687+git-kkumar@users.noreply.github.com>
2024-12-01 22:32:06 +0530

    updated mapstruct dependency and mapper

--- src/main/java/com/restaurant/restaurantlisting/entity/Restaurant.java
+++ src/main/java/com/restaurant/restaurantlisting/entity/Restaurant.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "restaurant")
 public class Restaurant {
 	@Id
--- src/main/java/com/restaurant/restaurantlisting/mapper/RestaurantMapper.java
+++ src/main/java/com/restaurant/restaurantlisting/mapper/RestaurantMapper.java
@@ -1,12 +1,14 @@
 package com.restaurant.restaurantlisting.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
 import com.restaurant.restaurantlisting.dto.RestaurantDTO;
 import com.restaurant.restaurantlisting.entity.Restaurant;
 
-@Mapper
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
 	
 	RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
