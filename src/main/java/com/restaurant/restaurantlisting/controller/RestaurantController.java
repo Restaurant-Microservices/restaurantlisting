603b7becf04a7926e720bd4050d3a0b653349c6f
git-kkumar <77931687+git-kkumar@users.noreply.github.com>
2024-12-04 22:58:06 +0530

    new api added

--- src/main/java/com/restaurant/restaurantlisting/controller/RestaurantController.java
+++ src/main/java/com/restaurant/restaurantlisting/controller/RestaurantController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,12 @@ public class RestaurantController {
 	@GetMapping("/fetchAllRestaurants")
 	public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants(){
 		List<RestaurantDTO> allRestaurants = restaurantService.fetchAllRestaurants();
+		return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
+	}
+	
+	@GetMapping("/fetchRestaurantById/{restaurantId}")
+	public ResponseEntity<RestaurantDTO> fetchRestaurantById(@PathVariable Integer restaurantId){
+		RestaurantDTO allRestaurants = restaurantService.fetchRestaurantById(restaurantId);
 		return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
 	}
 	
